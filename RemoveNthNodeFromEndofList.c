549e8b73bce1ba44022e062c4a47ee31ed06af24
Amirbahador Biglarbegian <Bahador_biglar@yahoo.com>
2015-05-07 17:35:41 -0700

    Remove Nth Node From End of List
    Updated!

--- RemoveNthNodeFromEndofList.c
+++ RemoveNthNodeFromEndofList.c
@@ -1,16 +1,43 @@
-int reverse(int x) {
-    int firstDigit, Reverse = 0, sign;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
+    if (head==NULL)
+        return head;
+    if (n<0)
+        return head;
+    struct ListNode* pp1, *p1, *lastNode;
     
-    sign = x<0?-1:1;
-    x=abs(x);
-    
-    while (x>0){
-        firstDigit = x%10;
-        x=x/10;
-        
-        if ( Reverse>214748364)
-            return 0;
-        Reverse=Reverse*10+firstDigit;
+    int i=1;
+    p1=head;
+    lastNode=head;
+    while ((i<n) && (lastNode!= NULL)){
+        lastNode=lastNode->next;
+        i++;
     }
-    return Reverse*sign;
+    
+    if (i<n)
+        return head;
+    
+    if (lastNode->next==NULL){
+        p1=head->next;
+        free(head);
+        return p1;
+    }
+    
+    pp1=head;
+    p1=p1->next;
+    lastNode=lastNode->next;
+    while (lastNode->next!=NULL){
+        lastNode=lastNode->next;
+        p1=p1->next;
+        pp1=pp1->next;
+    }
+    pp1->next=p1->next;
+    free(p1);
+    return head;
 }
